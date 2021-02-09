064c20311330965a1d97df20d1f6bda11a23d1ad
gabecbo97 <gabrielcbo@gmail.com>
2021-02-09 19:03:10 -0300

    done

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,7 +3,16 @@ import "./Calculator.css";
 import Button from "../components/Buttons";
 import Display from "../components/Display";
 
+const initialState = {
+  displayValue: "0",
+  cleanDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
 export default class Calculator extends Component {
+  state = { ...initialState };
   constructor(props) {
     super(props);
     this.clearMemory = this.clearMemory.bind(this);
@@ -11,18 +20,57 @@ export default class Calculator extends Component {
     this.addDigit = this.addDigit.bind(this);
   }
   clearMemory() {
-    console.log("limpar");
+    this.setState({ ...initialState });
   }
   setOperation(operation) {
-    console.log(operation);
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, cleanDisplay: true });
+    } else {
+      const equals = operation === '=';
+      const currentOperation = this.state.operation;
+
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        cleanDisplay: !equals,
+        values,
+      });
+    }
   }
   addDigit(n) {
-    console.log(n);
+    if (n === "." && this.state.displayValue.includes(".")) {
+      return;
+    }
+    const clearDisplay =
+      this.state.displayValue === "0" || this.state.cleanDisplay;
+
+    const currentValue = clearDisplay ? "" : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, cleanDisplay: false });
+
+    if (n !== ".") {
+      const i = this.state.current;
+      const newValue = parseInt(displayValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+      this.setState({ values });
+    }
   }
+
   render() {
     return (
       <div className="calculator">
-        <Display value={100} />
+        <Display value={this.state.displayValue} />
         <Button label="AC" click={this.clearMemory} triple />
         <Button label="/" click={this.setOperation} operation />
         <Button label="7" click={this.addDigit} />
@@ -38,7 +86,7 @@ export default class Calculator extends Component {
         <Button label="3" click={this.addDigit} />
         <Button label="+" click={this.setOperation} operation />
         <Button label="0" click={this.addDigit} double />
-        <Button label="." click={this.setOperation} />
+        <Button label="." click={this.addDigit} />
         <Button label="=" click={this.setOperation} operation />
       </div>
     );
